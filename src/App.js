558147a291b2529a7a469c4323fbaf2ebeab2551
Eder Ramirez <eder.ramirez87@gmail.com>
2019-05-14 17:04:45 -0500

    setting up production grade work-flow

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          EDEr! jojojo
         </a>
       </header>
     </div>
